f828fff038a8ed0067dc66c291ac2fea98e6d6b0
JacksonWhite4725 <jawh4725@colorado.edu>
2021-11-13 22:45:44 -0700

    Added basic component structure, fixed index.html styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import Header from './components/header/Header';
+import About from './components/aboutme/About';
+import Portfolio from './components/portfolio/Portfolio';
+import Footer from './components/footer/Footer';
+
 function App() {
   return (
-    <div className="app">
-      Hello World
+    <div className='app'>
+      <Header/>
+      <div className='sections'>
+        <About/>
+        <Portfolio/>
+        <Footer/>
+      </div>
     </div>
   );
 }
