0417cb1dde0f85d3eaaa01fdb6128a203fd658cb
JacksonWhite4725 <jawh4725@colorado.edu>
2021-11-15 23:42:15 -0700

    Added resume component

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import './Resume.css';
+import resume from '../../images/Jackson_White_Resume.pdf';
+import { init } from 'ityped';
+import { useEffect, useRef } from 'react';
 
 export default function Resume() {
+    const textRef = useRef();
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: false,
+            typeSpeed: 90,
+            backDelay: 5000,
+            strings: ['HTML | JavaScript | CSS | MongoDB | MySQL | Express | React | Node | Python | Digital Marketing | Brewing | Data Analytics | 中文 |']
+        })
+    }, [])
     return(
         <div className='resume' id='resume'>
+            <h1>Resume</h1>
+            <div className='skills'>
+                <h3><span ref={textRef}></span></h3>
+            </div>
+            <div className='resume-download'>
+                <a href={resume} download='Jackson_White_Resume'>
+                    <h2>Download My Resume</h2>
+                </a>
+            </div>
         </div>
     );
 };
